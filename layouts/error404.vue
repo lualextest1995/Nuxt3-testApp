a4df4d8a61ec7c938de0cd98c5b0f0aa977e632b
Alex <z0809777@gmail.com>
2023-11-19 20:03:13 +0800

    add 404 page

---
+++ layouts/error404.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.container {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: rgb(184, 239, 239);
+}
+</style>
---
+++ pages/[...slug].vue
@@ -0,0 +1,39 @@
+<template>
+  <el-card class="box-card">
+    <div class="error-img">
+      <img src="../assets/404.jpg" alt="" />
+    </div>
+    <NuxtLink to="/">回首頁</NuxtLink>
+  </el-card>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  // middleware: ['error-page-rules'],
+  layout: 'error404'
+})
+</script>
+
+<style lang="scss" scoped>
+.box-card {
+  text-align: center;
+  width: 500px;
+  a {
+    color: black;
+    font-size: 30px;
+    transition: all 0.5s ease;
+    &:hover {
+      color: rgb(202, 89, 89);
+    }
+  }
+}
+.error-img {
+  width: 500px;
+  height: 500px;
+  img {
+    object-fit: cover;
+    height: 100%;
+    width: 100%;
+  }
+}
+</style>
